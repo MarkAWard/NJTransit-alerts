7fa2ebed0597d715bd5d85e9ebf7a8df5e5a0ff2
MarkAWard <markmaw@gmail.com>
2014-09-29 00:25:07 -0400

    print

--- send_txt.py
+++ send_txt.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
    
     if alert != "":
     
-        ACCOUNT_SID = "AC410387af155b3f5e8d5bcd9f95ab0ca4" 
-        AUTH_TOKEN = "8f4b870973aedc7f54fdcbf9586a661e" 
+        ACCOUNT_SID = "" 
+        AUTH_TOKEN = "" 
         
         client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
         
@@ -31,3 +31,4 @@ if __name__ == "__main__":
         with open("log.txt", 'a') as l:
             l.write(str(datetime.now()) + '\t' + str(message.sid) + '\t' + title + '\t' + alert + '\n')
 
+        print str(datetime.now()) + '\t' + alert + '\n'
